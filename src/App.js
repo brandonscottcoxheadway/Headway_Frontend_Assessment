7fe13c8d124d753b56760a8bf1a265181e7936ac
brandonscottcoxheadway <104935167+brandonscottcoxheadway@users.noreply.github.com>
2022-05-04 14:21:48 -0600

    Styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="Border">
+        <div className="Background">
+          <h1>Shorten Your Links</h1>
+          <form>
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              placeholder="Make your links shorter"
+            />
+            <input type="submit" value="Shorten" />
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
